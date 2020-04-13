39310c4491b0173e043ee83f3561da54899258e7
ai-clown <1659448936@qq.com>
2020-04-13 22:55:39 +0800

    new

---
+++ AI-Clown/src/main/java/com/hd/AIClownRunApp.java
@@ -0,0 +1,12 @@
+package com.hd;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIClownRunApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AIClownRunApp.class,args);
+    }
+}
